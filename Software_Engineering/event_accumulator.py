0d6b3e3e966c53ef65fb7e60ce0b87190acef871
Muhammet USLU <81621143+uslumt@users.noreply.github.com>
2022-09-16 19:00:05 +0200

    Update event_accumulator.py
    assertion check

--- Software_Engineering/event_accumulator.py
+++ Software_Engineering/event_accumulator.py
@@ -1,8 +1,17 @@
-def convert_to_binary(width, height, pixels):
+from array import array
+import numpy as np
+import inspect
 
-    collected_event = np.full(( max(width) + 1, max(height) + 1 ), 0, dtype=int)
-    coordinates = list(zip(width, height))
-    rows, cols = zip(*coordinates)
-    collected_event[rows, cols] = pixels
-    
-    return collected_event
+def convert_to_binary(width , height, pixel):
+        for i in inspect.getfullargspec(convert_to_binary).args: 
+            indx = inspect.getfullargspec(convert_to_binary).args.index(i)
+            i_ = inspect.getfullargspec(convert_to_binary).args.__getitem__(indx)
+            i = eval(i_)
+            assert isinstance(i, (list, array, str, np.ndarray)),f"Type of {i_} list or array expected, got: {type(i)}" 
+
+        collected_event = np.full((len((width)),len((height))), 0, dtype=int)
+        coordinates = list(zip(width, height))
+        rows, cols = zip(*coordinates)
+        collected_event[rows, cols] = pixel
+        
+        return collected_event
